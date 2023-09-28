4fed53d211fbeb8a43b459e8924c5d9224d956c5
Shyamin Ayesh <me@shyamin.com>
2023-09-28 20:47:17 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/vayor/models
+
+go 1.21.0
+
+require gorm.io/plugin/soft_delete v1.2.1
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	gorm.io/gorm v1.23.0 // indirect
+)
---
+++ users.go
@@ -0,0 +1,19 @@
+package models
+
+import "gorm.io/plugin/soft_delete"
+
+type Users struct {
+	ID        uint64                `gorm:"column:id;primaryKey"`
+	FirstName string                `gorm:"column:name_first;default:null"`
+	LastName  string                `gorm:"column:name_last;default:null"`
+	Email     string                `gorm:"column:email;index:idx_email,unique;not null"`
+	Password  string                `gorm:"column:password;not null"`
+	JWTSecret string                `gorm:"column:jwt_secret;not null"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null"`
+}
+
+func (Users) TableName() string {
+	return "users"
+}
