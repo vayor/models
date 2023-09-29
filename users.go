4636193d867318c8a5e973085f8c49b3acfeff34
Shyamin Ayesh <me@shyamin.com>
2023-09-29 20:09:53 +0530

    fine tune model definitions

--- users.go
+++ users.go
@@ -4,11 +4,11 @@ import "gorm.io/plugin/soft_delete"
 
 type Users struct {
 	ID        uint64                `gorm:"column:id;primaryKey"`
-	Email     string                `gorm:"column:email;index:idx_email,unique;not null"`
-	Password  string                `gorm:"column:password;not null"`
+	Email     string                `gorm:"column:email;type:varchar(250);index:idx_email,unique;not null"`
+	Password  string                `gorm:"column:password;type:varchar(60);not null"`
 	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
-	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null;index:idx_deleted_at"`
 }
 
 func (Users) TableName() string {
