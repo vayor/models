7b3c814e82b26adad5bc833f0661093a6a7e33ed
Shyamin Ayesh <me@shyamin.com>
2023-09-29 17:01:47 +0530

    Model: Audit

---
+++ audit.go
@@ -0,0 +1,15 @@
+package models
+
+import "gorm.io/plugin/soft_delete"
+
+type Audit struct {
+	ID        uint64                `gorm:"column:id;primaryKey"`
+	Message   string                `gorm:"column:message"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null"`
+}
+
+func (Audit) TableName() string {
+	return "audit"
+}
