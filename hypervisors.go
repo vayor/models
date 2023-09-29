ede065f9b2578799b07cfeffbd34ed80f108c7f5
Shyamin Ayesh <me@shyamin.com>
2023-09-29 21:23:04 +0530

    Model: Hypervisors

---
+++ hypervisors.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/plugin/soft_delete"
+
+type Hypervisors struct {
+	ID        uint64                `gorm:"column:id;primaryKey"`
+	Host      string                `gorm:"column:host;type:varchar(250);not null"`
+	Port      uint16                `gorm:"column:port;not null"`
+	Secret    string                `gorm:"column:secret;type:varchar(128);not null"`
+	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null;index:idx_deleted_at"`
+}
+
+func (Hypervisors) TableName() string {
+	return "hypervisors"
+}
