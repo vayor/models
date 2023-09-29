26968efdd68c7c2e789280e9d3ed62338b6608ca
Shyamin Ayesh <me@shyamin.com>
2023-09-29 20:03:17 +0530

    define scope type

--- tokens.go
+++ tokens.go
@@ -5,7 +5,7 @@ import "gorm.io/plugin/soft_delete"
 type Tokens struct {
 	ID        uint64                `gorm:"column:id;primaryKey"`
 	UserID    uint64                `gorm:"column:user_id"`
-	Scope     string                `gorm:"column:scope"`
+	Scope     string                `gorm:"column:scope;type:enum('internal','external')"`
 	Secret    string                `gorm:"column:secret;type:varchar(36)"`
 	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
