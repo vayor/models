5be1da666f896640a698477a1d152f40023e86c1
Shyamin Ayesh <me@shyamin.com>
2023-10-03 21:29:25 +0530

    add hypervisor information

--- hypervisors.go
+++ hypervisors.go
@@ -8,6 +8,9 @@ type Hypervisors struct {
 	Port      uint16                `gorm:"column:port;not null"`
 	Secret    string                `gorm:"column:secret;type:varchar(128);not null"`
 	Region    string                `gorm:"column:region;type:enum('ap-sg-1');default:null"`
+	CPU       uint64                `gorm:"column:cpu;default:null"`
+	Memory    uint64                `gorm:"column:memory;default:null"`
+	Storage   uint64                `gorm:"column:storage;default:null"`
 	CreatedAt int64                 `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt int64                 `gorm:"column:updated_at;autoUpdateTime"`
 	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;default:null;index:idx_deleted_at"`
